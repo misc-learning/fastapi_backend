b37ab63e50fe3866d068163cc4fe0037805792c7
Muhammad Salman Shahid <muhammad-salman.shahid@grenoble-inp.fr>
2026-03-05 11:19:29 +0100

    path parameters

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
+from typing import Any
+
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
 
-@app.get("/shipment")
-def get_shipment():
+@app.get("/shipment/{id}")
+def get_shipment(id: int) -> dict[str, Any]:
     """Get shipment.
 
     Returns:
         _type_: _description_
 
     """
-    return {"article": "notebook", "status": "in transit"}
+    return {"id": id, "weight": 1.2, "article": "notebook", "status": "in transit"}
 
 
 @app.get("/scalar", include_in_schema=False)
